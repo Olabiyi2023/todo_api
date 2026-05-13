0572a20ec16259f7605eb029c99055bcab985006
Olabiyi2023 <abassolabiyi3@gmail.com>
2026-05-13 12:19:43 +0100

    first commit

---
+++ controller/todoController.js
@@ -0,0 +1,75 @@
+const TodoModel = require('../model/todoModel');
+
+//crud
+const getAllTodos = async (req, res) => {
+    try {
+        const todos = await TodoModel.find();
+        return res.status(200).json({
+            message: 'All Todos',
+            data: todos
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching todos", error: error.message });
+    }
+};
+
+const getOneTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await TodoModel.findById(id);
+        return res.status(200).json({
+            message: 'Todo found',
+            data: todo
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching todo", error: error.message });
+    }
+};
+
+const createTodo = async (req, res) => {
+    try {
+        const { title, details } = req.body;
+        const newTodo = await TodoModel.create({ title, details });
+        return res.status(201).json({
+            message: 'Todo created',
+            data: newTodo
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error creating todo", error: error.message });
+    }
+};
+
+const updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+       
+        const todo = await TodoModel.findByIdAndUpdate(id, { completed: true }, { new: true });
+        return res.status(200).json({
+            message: 'Todo updated',
+            data: todo
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error updating todo", error: error.message });
+    }
+}; 
+
+const deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await TodoModel.findByIdAndDelete(id);
+        return res.status(200).json({
+            message: 'Todo deleted'
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting todo", error: error.message });
+    }
+};
+
+module.exports = {
+    getAllTodos,
+    getOneTodo,
+    createTodo,
+    updateTodo,
+    deleteTodo
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const moongoose = require('mongoose');
+const live_url = 'mongodb://dev_olabiyi:test1234@ac-fzah8uz-shard-00-00.0c8djyq.mongodb.net:27017,ac-fzah8uz-shard-00-01.0c8djyq.mongodb.net:27017,ac-fzah8uz-shard-00-02.0c8djyq.mongodb.net:27017/?ssl=true&replicaSet=atlas-2t4hxo-shard-0&authSource=admin&appName=Cluster0';
+const local_url = 'mongodb://localhost:27017/userDB';
+const router = require('./routes/todoRoutes');
+
+moongoose.connect(live_url)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+app.use("/todos", router);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
